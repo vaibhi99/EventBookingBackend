5ab3d51dd58f0145a1fcb240d27c38771b410887
sourabh17gupta <mkktlguptas@gmail.com>
2025-07-01 18:51:50 +0530

    Update Auth.js

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 exports.AuthN = async (req, res, next) => {
   try {
-    const token = req.cookies.token || req.headers.authorization.replace("Bearer ", "");
+    const token = req.cookies.token;
       
     if (!token) {
       return res.status(401).json({
